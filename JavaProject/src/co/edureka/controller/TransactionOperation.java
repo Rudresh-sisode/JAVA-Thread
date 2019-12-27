c905c150e6bef1adc357d7124c503ce61b8f41c9
Rudresh Sisode <rudranrajput@gmail.com>
2019-12-27 19:14:06 +0530

    My Edureka Java Final Project in Bank Transaction

---
+++ JavaProject/src/co/edureka/controller/TransactionOperation.java
@@ -0,0 +1,32 @@
+package co.edureka.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet({ "/TransactionOperation", "/Transaction" })
+public class TransactionOperation extends HttpServlet {
+
+    
+	
+	public void init(ServletConfig config) throws ServletException {
+	
+	}
+
+
+	public void destroy() {
+
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		
+	}
+
+}
---
+++ JavaProject/src/co/edureka/model/AddUser.java
@@ -0,0 +1,25 @@
+package co.edureka.model;
+
+public class AddUser {
+	public String name;
+	public String date;
+	public String address;
+	public String email;
+	public String accountType;
+	
+	
+	
+	public AddUser(String name, String date, String address, String email, String accountType) {
+		
+		this.name = name;
+		this.date = date;
+		this.address = address;
+		this.email = email;
+		this.accountType = accountType;
+	}
+	
+	
+	
+	
+
+}
